0d3ea6a3bc29e1080b46193da257564eb9d4868f
j4ckson185 <jackson_division@hotmail.com>
2024-07-20 17:17:41 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,28 +11,6 @@ self.addEventListener('fetch', (event) => {
     console.log('Fetch interceptado para:', event.request.url);
 });
 
-self.addEventListener('push', (event) => {
-    const data = event.data.json();
-    console.log('Push recebido:', data);
-
-    const options = {
-        body: data.body,
-        icon: 'https://i.ibb.co/jZ6rbSp/logo-cabana.png',
-        badge: 'https://i.ibb.co/jZ6rbSp/logo-cabana.png',
-    };
-
-    event.waitUntil(
-        self.registration.showNotification(data.title, options)
-    );
-});
-
-self.addEventListener('notificationclick', (event) => {
-    event.notification.close();
-    event.waitUntil(
-        clients.openWindow(event.notification.data.url)
-    );
-});
-
 self.addEventListener('message', (event) => {
     if (event.data && event.data.type === 'PLAY_SOUND') {
         playNotificationSound();
